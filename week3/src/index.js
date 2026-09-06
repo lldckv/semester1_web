cdde236fca4d0a208bd1a740e0a4b5eec934859d
lldckv <lldckv@gmail.com>
2026-09-06 20:13:30 +0300

    week3 init

---
+++ week3/src/index.js
@@ -0,0 +1,12 @@
+import htmlContent from './week3.html';
+
+export default {
+  async fetch(request, env, ctx) {
+    const url = new URL(request.url);
+    return new Response(htmlContent, {
+      headers: {
+        'content-type': 'text/html;charset=UTF-8',
+      },
+    });
+  },
+};
